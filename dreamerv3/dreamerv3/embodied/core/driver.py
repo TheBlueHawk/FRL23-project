71de26eeb0b2e0581b57cae7676e121e1ef4067d
Chris <mullerchristophe21@gmail.com>
2023-05-19 16:18:51 +0200

    comments driver and train chatgpt

--- dreamerv3/dreamerv3/embodied/core/driver.py
+++ dreamerv3/dreamerv3/embodied/core/driver.py
@@ -42,35 +42,53 @@ class Driver:
       step, episode = self._step(policy, step, episode)
 
   def _step(self, policy, step, episode):
+    # Assertion: Check that the lengths of all actions are consistent with the number of environments
     assert all(len(x) == len(self._env) for x in self._acts.values())
+    
+    # Action Processing: Filter actions and perform an action step in the environment
     acts = {k: v for k, v in self._acts.items() if not k.startswith('log_')}
     obs = self._env.step(acts)
     obs = {k: convert(v) for k, v in obs.items()}
     assert all(len(x) == len(self._env) for x in obs.values()), obs
+    
+    # Policy Execution: Get actions from the policy function based on the observations and state
     acts, self._state = policy(obs, self._state, **self._kwargs)
     acts = {k: convert(v) for k, v in acts.items()}
+    
+    # Handling 'is_last' Observations: Adjust actions for terminated environments
     if obs['is_last'].any():
-      mask = 1 - obs['is_last']
-      acts = {k: v * self._expand(mask, len(v.shape)) for k, v in acts.items()}
+        mask = 1 - obs['is_last']
+        acts = {k: v * self._expand(mask, len(v.shape)) for k, v in acts.items()}
     acts['reset'] = obs['is_last'].copy()
     self._acts = acts
+    
+    # Transition Processing: Merge observations and actions into a transition dictionary
     trns = {**obs, **acts}
+    
+    # Handling 'is_first' Observations: Clear episode dictionaries for new episodes
     if obs['is_first'].any():
-      for i, first in enumerate(obs['is_first']):
-        if first:
-          self._eps[i].clear()
+        for i, first in enumerate(obs['is_first']):
+            if first:
+                self._eps[i].clear()
+    
+    # Environment-wise Processing: Process transitions for each environment
     for i in range(len(self._env)):
-      trn = {k: v[i] for k, v in trns.items()}
-      [self._eps[i][k].append(v) for k, v in trn.items()]
-      [fn(trn, i, **self._kwargs) for fn in self._on_steps]
-      step += 1
+        trn = {k: v[i] for k, v in trns.items()}
+        [self._eps[i][k].append(v) for k, v in trn.items()]
+        [fn(trn, i, **self._kwargs) for fn in self._on_steps]
+        step += 1
+    
+    # Handling 'is_last' Observations (Episode Completion): Call episode callbacks for completed episodes
     if obs['is_last'].any():
-      for i, done in enumerate(obs['is_last']):
-        if done:
-          ep = {k: convert(v) for k, v in self._eps[i].items()}
-          [fn(ep.copy(), i, **self._kwargs) for fn in self._on_episodes]
-          episode += 1
+        for i, done in enumerate(obs['is_last']):
+            if done:
+                ep = {k: convert(v) for k, v in self._eps[i].items()}
+                [fn(ep.copy(), i, **self._kwargs) for fn in self._on_episodes]
+                episode += 1
+    
+    # Return updated step and episode counters
     return step, episode
+
 
   def _expand(self, value, dims):
     while len(value.shape) < dims:
