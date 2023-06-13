b03c4856a266a1bb49b8492021fe26e3b8113f9f
Kevin <kblin@outlook.com>
2023-06-13 16:53:26 +0200

    PETS setup

---
+++ pets/pets_env_setup.sh
@@ -0,0 +1,6 @@
+# srun --gpus=1 --ntasks=4 --mem-per-cpu=4G --pty bash
+conda create -n pets python=3.6
+conda activate pets
+pip install -r requirements.txt
+module load gcc/4.8.5 cuda/10.0.130 cudnn/7.4 eth_proxy
+python scripts/mbexp.py -env cartpole
